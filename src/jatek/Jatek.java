132008b247e20a18bec50b6bf6c7a4d0cec2afd0
veres.marcell <veres.marcell@host.docker.internal>
2023-12-11 14:56:49 +0100

    +teszt()

--- src/jatek/Jatek.java
+++ src/jatek/Jatek.java
@@ -3,7 +3,17 @@ package jatek;
 public class Jatek {
 
     public static void main(String[] args) {
+        teszt();
+    }
 
+    private static void teszt() {
+        Karakter karakter = new Karakter();
+        karakter.felvesz(new Targy("sus", "amongus"));
+        karakter.felvesz(new Targy("sus2", "amongus 2"));
+        karakter.felvesz(new Targy("sus", "amongus 4"));
+        karakter.felvesz(new Targy("sus", "amongus 3"));
+        System.out.println(karakter.hasznal("sus",2,false));
+        System.out.println(karakter.hasznal("sus",2,false));
     }
     
 }
