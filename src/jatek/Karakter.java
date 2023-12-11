a765414c766e0954def2d1a1772cb49ee85cf5ac
veres.marcell <veres.marcell@host.docker.internal>
2023-12-11 13:43:53 +0100

    +targy, felvesz()

--- src/jatek/Karakter.java
+++ src/jatek/Karakter.java
@@ -7,11 +7,20 @@ import java.util.Random;
 public class Karakter {
     Random rnd;
     int eletero, ugyesseg, szerencse;
-    ArrayList<String> felszereles;
+    ArrayList<Targy> felszereles;
     
     public Karakter() {
         eletero= rnd.nextInt(1, 7)+rnd.nextInt(1, 7)+12;
         ugyesseg= rnd.nextInt(1, 7)+6;
         szerencse= rnd.nextInt(1, 7)+6;
+    }
+    
+    public void felvesz(Targy targy){
+        felszereles.add(targy);
+    }
+    
+    public void felvesz(Targy targy, int db){
+        for (int ix = 0; ix < db; ix++)
+            felvesz(targy);
     }
 }
